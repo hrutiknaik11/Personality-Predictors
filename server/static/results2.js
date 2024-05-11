c34eed8195edfbe97ebf93688214a859ccfcb17c
hrutiknaik11 <hrutupn@gmail.com>
2024-05-11 17:33:30 -0400

    First Commit

---
+++ server/static/results2.js
@@ -0,0 +1,2 @@
+var follower_data={'0': 'INFJ', '1': 'INFJ', '2': 'INFJ', '3': 'ENTJ', '4': 'INFJ'}
+var follower_ids={'0': 'PradipD43296410', '1': 'Nikhilabef', '2': 'JMistry45', '3': 'sheljagina', '4': 'VijayAbef'}
